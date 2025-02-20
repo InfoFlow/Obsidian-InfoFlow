8230a1fb49b57751a343ed3fc3bc1258c3e393c3
ian <ian.c@rockiestar.com>
2025-02-20 09:13:12 -0700

    Remove unused editor commands and sync modal
    * Deleted editor command for inserting template
    * Removed sync modal command
    * Cleaned up commented-out code in main plugin file

--- main.ts
+++ main.ts
@@ -131,28 +131,8 @@ export default class InfoFlowPlugin extends Plugin {
 		// 	}
 		// });
 		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: "editor-command",
-			name: "Insert template",
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection("Sample Editor Command");
-			},
-		});
+
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: "open-modal",
-			name: "Open sync modal",
-			checkCallback: (checking: boolean) => {
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					if (!checking) {
-						new InfoFlowSyncModal(this.app).open();
-					}
-					return true;
-				}
-			},
-		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new InfoFlowSettingTab(this.app, this));
